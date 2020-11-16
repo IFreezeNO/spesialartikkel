da524356467e891359a576af0ba212b6f5eb7ee9
IFreezeNO <46674863+IFreezeNO@users.noreply.github.com>
2020-11-16 13:45:26 +0100

    Add files via upload

---
+++ kilder/javascript/graph.js
@@ -0,0 +1,37 @@
+var ctx = document.getElementById('drukningsulykker').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['2014', '2015', '2016', '2017', '2018', '2019'],
+        datasets: [{
+            label: 'Drukningsulykker i 2020',
+            data: [3538, 3771, 5096, 3139, 2277, 1148],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
